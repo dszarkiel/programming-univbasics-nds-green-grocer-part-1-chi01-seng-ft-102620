ca41b141f29fb5e617af0dfb99e64bd9acaf8319
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-22 16:21:15 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -8,26 +8,34 @@ def find_item_by_name_in_collection(name, collection)
   end
 end
 
-
 def consolidate_cart(cart)
-  index = 0
-  new_cart = []
-
-  while index < cart.length do
-    item = find_item_by_name_in_collection(cart[index][:item], new_cart)
-    if item
-      new_cart_index = 0
-      while new_cart_index < new_cart.length do
-        if new_cart[new_cart_index][:item] === item[:item]
-          new_cart[new_cart_index][:count] += 1
-        end
-        new_cart_index += 1
+new_array = []
+i = 0
+while i < cart.length do
+  product = find_item_by_name_in_collection(cart[i][:item], new_array)
+  if product
+    new_array_i = 0
+    while new_array_i < new_array.length do
+      if new_array[new_array_i][:item] == product[:item]
+        new_array[new_array_i][:count] += 1
       end
-    else
-      cart[index][:count] = 1
-      new_cart << cart[index]
+      new_array_i += 1
     end
-    index += 1
+  else
+    cart[i][:count] = 1
+    new_array << cart[i]
   end
-  new_cart
+  i += 1
+end
+new_array
+end
+
+
+
+
+  return new_array
+
+
+
+
 end
