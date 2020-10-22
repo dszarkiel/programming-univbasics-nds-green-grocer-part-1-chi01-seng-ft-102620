d7304c3c625d9c853274cbb54793d8959baedb1a
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-22 15:16:55 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,13 +1,13 @@
 def find_item_by_name_in_collection(name, collection)
   count = 0
-  needed_item = {}
+  #needed_item = {}
   while count < collection.length do
     if collection[count][:item] == name
-      needed_item = collection[count]
+      collection[count][:item]
   end
   count += 1
   end
-  needed_item
+  #needed_item
 end
 
 def consolidate_cart(cart)
