bd96a5c85115625e98bb3d243ac9059dbfb1b96d
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-22 15:34:43 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -9,5 +9,12 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
+new_array = []
+turn = 0
+while turn < cart.length do
+  cart[turn][:count] = 1
+  new_array << cart[turn]
 
+
+end
 end
